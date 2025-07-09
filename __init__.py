7c9c534cfe393d067adc05c1a39d1d17363f238a
陈亮 <cody@51cashbox.com>
2025-07-09 11:37:37 +0800

    修复节点名称问题

--- __init__.py
+++ __init__.py
@@ -1,16 +1,22 @@
 import os
-import importlib
+import importlib.util
 
 NODE_CLASS_MAPPINGS = {}
 NODE_DISPLAY_NAME_MAPPINGS = {}
 
-nodes_path = os.path.join(os.path.dirname(__file__), "nodes")
+nodes_dir = os.path.join(os.path.dirname(__file__), "nodes")
 
-for filename in os.listdir(nodes_path):
+for filename in os.listdir(nodes_dir):
     if filename.endswith(".py") and filename != "__init__.py":
-        module_name = f"{__name__}.nodes.{filename[:-3]}"
-        module = importlib.import_module(module_name)
+        filepath = os.path.join(nodes_dir, filename)
+        module_name = filename[:-3]  # 不要带你的包名
 
+        # 动态加载模块（防止污染 __name__）
+        spec = importlib.util.spec_from_file_location(module_name, filepath)
+        module = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(module)
+
+        # 查找 *_CLASS_MAPPINGS
         for attr in dir(module):
             if attr.endswith("_CLASS_MAPPINGS"):
                 NODE_CLASS_MAPPINGS.update(getattr(module, attr))
